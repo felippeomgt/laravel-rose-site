072d2858bb501cc12d47cd9057d2bd696ac482af
Felippe Ribeiro <felippe.omgt@gmail.com>
2021-11-21 17:09:51 -0300

    more fixes, still missing domain registration to be able to send emails and receive donations

--- resources/views/partials/donate.blade.php
+++ resources/views/partials/donate.blade.php
@@ -9,32 +9,51 @@
     <div class="header big-padding">
         <h2>Donations</h2>
     </div>
-        <p>Ajude nos a manter nosso servidor funcionando!</p><br>
-        <p>Ao doar para o servidor você receberá pontos de ROSE CASH, esses pontos podem ser usados dentro do jogo para obter itens especiais!<br>
-        Os itens que podem ser adiquiridos com ROSE CASH são a maioria cosméticos, equipamentos, consumiveis e veículos. <br>
-            Por mais que alguns equipamentos possuam ótimos atributos, os itens que podem ser obtidos dentro do jogo vão ser sempre melhores do que os itens de CASH,<br> isso garante que o jogo não se torne "pague para vencer". </p>
-        <p> Você não é obrigado a doar, mas se fizer, estará ajudando a pagar os custos do servidor e trazer investimentos e melhorias para o jogo.
+        <div class="donate-section">
+            <h3>Ajude nos a manter nosso servidor funcionando!</h3><br>
+            <p class="small">Ao doar para o servidor você receberá pontos de ROSE CASH, esses pontos podem ser usados para obter itens especiais dentro do jogo!<br>
+                Os itens que podem ser adiquiridos com ROSE CASH são a maioria cosméticos, equipamentos, consumiveis e veículos. <br>
+                Por mais que alguns equipamentos possuam ótimos atributos, é possível obter itens melhores dentro do jogo sem nenhum custo,<br> nossa loja e jogo são bem balanceados, isso garante que o jogo não se torne "pague para vencer". </p>
+            <p class="small"> Você não é obrigado a doar, nosso servidor será sempre free to play, mas ao doar, estará ajudando a pagar os custos do servidor e contribuindo para trazer investimentos e melhorias para o nosso jogo.
+            <hr>
 
-    <form action="/payment" method="post">
+        </div>
 
-
-
-
-
-        <select id="points" name="points">
-            <option value="250">R$ 4,99 -> 250RC</option>
-            <option value="550">R$ 9,99 -> 550RC</option>
-            <option value="1250">R$ 19,99 -> 1250RC</option>
-            <option value="1600">R$ 24,99 -> 1600RC</option>
-			<option value="2700">R$ 39,99 -> 2700RC</option>
-            <option value="3600">R$ 49,99 -> 3600RC</option>
-            <option value="8000">R$ 99,99 -> 8000RC</option>
-            <option value="13000">R$ 149,99 -> 13000RC</option>
-			<option value="25000">R$ 249,99 -> 25000RC</option>
-
-            <input type="submit" value="submit">
-        </select>
-    </form>
+            <div class="container donation-container">
+                <h3 class="mb-4 text-white">Pacotes de valores:</h3>
+                <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 text-center">
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 5,00 -> 250RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 10,00 -> 550RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 20,00 -> 1250RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 25,00 -> 1600RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 40,00 -> 2700RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 50,00 -> 3600RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 100,00 -> 8000RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 150,00 -> 13000RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash">R$ 250,00 -> 25000RC</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 border cash" title="Para cada R$ 1,00 você receberá 50RC">R$ Outro valor -> ?RC</div>
+                    </div>
+                </div>
+            </div>
 @endsection
 
 
